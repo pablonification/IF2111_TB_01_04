6b9e71bce7262928d3c840ac9c0e13da911fd080
veliciagabriell <18223085@std.stei.itb.ac.id>
2024-12-17 22:52:58 +0700

    add: fix handling

--- include/features/DNA2.h
+++ include/features/DNA2.h
@@ -9,10 +9,13 @@ const int gap = -1;
 
 boolean isDNAvalid(Word sequence);
 /* Mengembalikan true jika sequence merupakan DNA valid, yaitu hanya terdiri dari karakter A, T, C, dan G */
+
 int needlemanWunsch(Word ref, Word query, char *alignedRef, char *alignedQuery, int *alignedLength);
 /* Algoritma dynamic programming untuk menghitung skor dari kococokan sequence*/
+
 void traceback(Word ref, Word query, int **matrix, int m, int n, char *alignedRef, char *alignedQuery, int *alignedLength);
 /* Fungsi melakukan handling gap pada pencetakan alignedRef dan alignedQuery (sudah disejajarkan) */
+
 int deteksiKebocoranDNA();
 /* Fungsi utama untuk deteksi kebocoran DNA */
 
